fd33d14679d0a3ab1956dbd7d3aa31c8e45b0400
XM & Patrick David <student@epicodus.com>
2014-07-22 11:45:01 -0700

    You can press buttons to feed, play, and put your pet to sleep.

--- js/scripts.js
+++ js/scripts.js
@@ -21,29 +21,49 @@ var Tamagotchi = {
 
 $(document).ready(function(){
   $("form#Tamagotchi").submit(function (event) {
+
+    var currentPet;
     event.preventDefault();
 
     var newName = $("input#name").val();
     newName = Object.create(Tamagotchi);
     newName.initialize();
     newName.name = $("input#name").val();
+    $("div#toms-list").append("<p><span class='toggle'>"+ newName.name + "</span></p>");
 
-    $("#tom-name").text(newName.name);
+    // $("span.toggle").last().click(function() {
+    // currentPet = newName;
+    //   $("div.row").empty();
+    //   $("div#food").text(currentPet.foodLevel);
+    //   $("div#sleep").text(currentPet.sleepLevel);
+    //   $("div#activity").text(currentPet.activityLevel);
+    // });
 
     var intervalID = setInterval(function() {
       newName.timePasses();
-    $("#food").text(newName.foodLevel);
-    $("#sleep").text(newName.sleepLevel);
-    if ((newName.foodLevel < 0) || (newName.sleepLevel < 0)) {
-      $("#output").text("You are not ready for life");
-      clearInterval(intervalID);
-      console.log(newName.isAlive())
-    }
-    $("#activity").text(newName.activityLevel);
-       }, 3000);
+      $("#food").text(newName.foodLevel);
+      $("#sleep").text(newName.sleepLevel);
+      if ((newName.foodLevel < 0) || (newName.sleepLevel < 0)) {
+        $("#output").text("You are not ready for life");
+        clearInterval(intervalID);
+        console.log(newName.isAlive())
+      }
+      $("#activity").text(newName.activityLevel);
+         }, 3000);
 
 
 
+    $(".feed-button").click(function(event) {
+      newName.foodLevel = newName.foodLevel + 1;
+    });
+
+    $(".nap-button").click(function(event) {
+      newName.sleepLevel = newName.sleepLevel + 1;
+    });
+
+    $(".play-button").click(function(event) {
+      newName.activityLevel = newName.activityLevel + 1;
+    });
   });
 });
 
